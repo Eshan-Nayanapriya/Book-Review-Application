01fa5a4b75966fa79b2a06165471e348e3a62a32
Eshan Nayanapriya <161910749+Eshan-Nayanapriya@users.noreply.github.com>
2024-11-21 14:30:19 +0530

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,50 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import morgan from "morgan";
+import dbConnect from "../src/config/dbConnection.js";
+
+const app = express();
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+
+const PORT = process.env.SERVER_PORT || 3001;
+
+app.get("/", (req, res) => {
+  res.send({ message: "Server is running at port " + PORT });
+});
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  })
+);
+
+const startServer = async () => {
+  try {
+    const dbConnection = await dbConnect();
+    if (dbConnection === true) {
+      try {
+        app.listen(PORT, () => {
+          console.info(`🚀 Server is running on port ${PORT}`);
+        });
+      } catch (error) {
+        console.error("❌ Cannot start the server: ", error);
+      }
+    }
+  } catch (error) {
+    console.error("❌ Cannot start the server: ", error);
+  }
+};
+
+startServer();
