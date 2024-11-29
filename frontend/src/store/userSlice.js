be9cfea5185efeb3623c9db093fd8088737e2b7e
Eshan Nayanapriya <161910749+Eshan-Nayanapriya@users.noreply.github.com>
2024-11-29 13:01:57 +0530

    Fetch user details on app load and update Redux store with user information

--- frontend/src/store/userSlice.js
+++ frontend/src/store/userSlice.js
@@ -4,18 +4,22 @@ const initialValue = {
   _id: "",
   name: "",
   email: "",
+  avatar : ""
 };
 
 const userSlice = createSlice({
   name: "user",
-  initialState : initialValue,
+  initialState: initialValue,
   reducers: {
     setUserLoginDetails: (state, action) => {
-      state = { ...action.payload };
+      state._id = action.payload._id;
+      state.name = action.payload.name;
+      state.email = action.payload.email;
+      state.avatar = action.payload.avatar;
     },
   },
 });
 
-export const {setUserLoginDetails} = userSlice.actions;
+export const { setUserLoginDetails } = userSlice.actions;
 
 export default userSlice.reducer;
